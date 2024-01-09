65eb68f3c8204be6ea89d0a5ab30aeb3417083b7
plsMoment <kegor2002@gmail.com>
2024-01-09 15:22:58 +0400

    minor fixes

--- t14.go
+++ t14.go
@@ -7,7 +7,7 @@ import (
 
 // typeof returns string that determine type of parameter v.
 // Detects types: int, string, bool and any chan
-func typeof(v any) string {
+func reflectTypeof(v any) string {
 	switch reflect.TypeOf(v).Kind().String() {
 	case "int":
 		return "int"
@@ -16,7 +16,22 @@ func typeof(v any) string {
 	case "bool":
 		return "bool"
 	case "chan":
-		return "channel"
+		return "chan"
+	default:
+		return ""
+	}
+}
+
+func typeof(v any) string {
+	switch v.(type) {
+	case int:
+		return "int"
+	case string:
+		return "string"
+	case bool:
+		return "bool"
+	case chan bool:
+		return "chan"
 	default:
 		return ""
 	}
@@ -27,6 +42,11 @@ func main() {
 	str := "aboba"
 	f := true
 	var ch chan bool
+	fmt.Println(reflectTypeof(a))
+	fmt.Println(reflectTypeof(str))
+	fmt.Println(reflectTypeof(f))
+	fmt.Println(reflectTypeof(ch))
+	fmt.Println("--------------")
 	fmt.Println(typeof(a))
 	fmt.Println(typeof(str))
 	fmt.Println(typeof(f))
--- t15.go
+++ t15.go
@@ -13,13 +13,31 @@ func createHugeString(n int) string {
 // In original code were two main problems.
 // First - slice of string may not contain 100 characters if they are not ASCII.
 // Second - slice of huge string don't allow GC sweep huge string, therefore we use our memory less efficiently.
-func someFunc() {
+func someFuncRune() {
 	v := createHugeString(1 << 10)
 	mv := []rune(v)
 	justString = string(mv[:100])
 }
 
+// if we want to copy just the number of bytes
+func someFuncCopy() {
+	v := createHugeString(1 << 10)
+	b := make([]byte, 100)
+	copy(b, v[:100])
+	justString = string(b)
+}
+
+// Same as upper with strings package
+func someFuncClone() {
+	v := createHugeString(1 << 10)
+	justString = strings.Clone(v[:100])
+}
+
 func main() {
-	someFunc()
+	someFuncRune()
+	println(justString)
+	someFuncCopy()
+	println(justString)
+	someFuncClone()
 	println(justString)
 }
